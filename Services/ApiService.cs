5c5e7863982c87adc3a094a007972aa2c77ac7c6
Chris <chris_oleary@hotmail.co.uk>
2020-07-08 16:07:07 +0100

    first commit

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -14,7 +14,8 @@ namespace WebApplication2.Services
             request.AddHeader("user-key", "fe22475530846f8a6934bdd84ea679ae");
             request.AddHeader("Content-Type", "application/json; charset=utf-8");
             request.AddHeader("Cookie", "__cfduid=d1f77e23764258e7eee5d78378b9457901594024789");
-            request.AddParameter("text/plain", "fields *; limit 10;", ParameterType.RequestBody);
+            // get all future dated xbox games
+            request.AddParameter("text/plain", "fields *, release_dates.*, screenshots.*, cover.*; \r\nwhere release_dates.platform = (49) & release_dates.date > 1594204300;\r\nsort release_dates.date desc;", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
 
             // needs to be list<> so square brackets are removed from json
